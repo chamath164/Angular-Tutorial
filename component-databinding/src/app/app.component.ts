1bf6bc506e853e7ed30170cab05e3dc220a8b91d
chamath7 <chamalakshan164@gmail.com>
2021-02-10 18:18:17 +0530

    first-commit

---
+++ component-databinding/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{ type: 'Server', name: 'TestServer', content: 'Just a server!' }];
+}
