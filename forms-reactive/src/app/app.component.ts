adabee3e299a5bc4b9cbca8b77c3a5ad3840ccaf
chamath7 <chamalakshan164@gmail.com>
2021-04-12 08:49:02 +0530

    reactive-forms

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,34 +1,53 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {validateAndRewriteCoreSymbol} from '@angular/compiler-cli/src/ngtsc/imports';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  projectForm: FormGroup;
+  forbiddenUsernames = ['Anna', 'Mark'];
 
-  // tslint:disable-next-line:typedef
-  ngOnInit() {
+  ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([])
     });
+
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('critical')
+    });
   }
-  // tslint:disable-next-line:typedef
-  onSubmit(){
+
+  onSubmit(): void {
     console.log(this.signupForm);
   }
 
-  onAddHobby(){
+  onAddHobby(): void {
     const control = new FormControl(null, Validators.required);
     (<FormArray> this.signupForm.get('hobbies')).push(control);
   }
 
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {nameIsForbidden: true};
+    }
+    return null;
+  }
+
+  onSaveProject(): void {
+    console.log(this.projectForm.value);
+  }
 }
