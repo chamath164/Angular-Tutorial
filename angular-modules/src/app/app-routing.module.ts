b80a78a1a4ecd609828e9fad374d7f2f22c9a397
chamath7 <chamalakshan164@gmail.com>
2021-04-22 12:15:57 +0530

    modules

--- angular-modules/src/app/app-routing.module.ts
+++ angular-modules/src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  // {
-  //   path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
-  // }
+  // lazy-loading
+  // {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)}
 ];
 
 @NgModule({
